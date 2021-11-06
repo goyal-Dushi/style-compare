563a42e3920ce547a9b4b5a5c892e6d55df06a33
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-06 17:18:40 +0530

    added typecontext values for style types

--- src/components/StyleToggleNav.jsx
+++ src/components/StyleToggleNav.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TypeContext } from "../App";
 import styles from "./styleToggleNav.module.css";
 
 function StyleToggleNav(props) {
+  const types = useContext(TypeContext);
+  const toggleStyle = (type) => {
+    if (type === "M") {
+      props.setStyleType("M");
+    } else if (type === "B") {
+      props.setStyleType("B");
+    }
+  };
   return (
     <div className={styles.mainContainer}>
       <div className={styles.styleContainer}>
-        {props?.types?.map((item, i) => (
-          <button key={i} className={styles.styleButton}>
+        {types?.map((item, i) => (
+          <button
+            onClick={() => toggleStyle(item)}
+            key={i}
+            className={styles.styleButton}>
             {item}
           </button>
         ))}
