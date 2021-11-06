64e15ee018f6af7d52e11baeba0fea71d37ff535
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-06 17:17:55 +0530

    destructured children from props

--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./displaybox.module.css";
 
-function DisplayBox({ show }) {
-  return <div className={styles.displayBox}>{show}</div>;
+function DisplayBox({ children }) {
+  return <div className={styles.displayBox}>{children}</div>;
 }
 
 export default DisplayBox;
