22b215cd7ff84063d8f31dea5c25e382a228e50c
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-07 12:32:53 +0530

    changed syntaxhighlighter style, added copy-to-clipboard, included copy alert snackbar

--- src/components/CodeBox.jsx
+++ src/components/CodeBox.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import { IconButton, Snackbar, Tooltip } from "@mui/material";
+import React, { useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { githubGist } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import styles from "./codebox.module.css";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function CodeBox({ snippet }) {
+  const [alert, setAlert] = useState(false);
+  const handleAlertClose = (event, reason) => {
+    if (reason === "clickaway") {
+      setAlert(false);
+      return;
+    }
+    setAlert(false);
+  };
   return (
-    <div className={styles.codebox}>
-      <SyntaxHighlighter className={styles.styleHighlighter} style={githubGist}>
-        {snippet}
-      </SyntaxHighlighter>
-    </div>
+    <>
+      <div className={styles.codebox}>
+        <CopyToClipboard
+          text={snippet}
+          onCopy={(text, result) => (result ? setAlert(true) : null)}>
+          <div style={{ position: "relative", width: "100%" }}>
+            <Tooltip title={"Copy"} arrow={true} placement={"left"}>
+              <IconButton
+                aria-label={"copy"}
+                style={{ position: "absolute", right: "0" }}
+                color={"primary"}
+                size={"large"}>
+                <ContentCopyIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </CopyToClipboard>
+        <SyntaxHighlighter className={styles.styleHighlighter} style={monokai}>
+          {snippet}
+        </SyntaxHighlighter>
+      </div>
+      <Snackbar
+        anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+        autoHideDuration={2000}
+        message={"Code copied to Clipboard!"}
+        open={alert}
+        onClose={handleAlertClose}
+      />
+    </>
   );
 }
 
