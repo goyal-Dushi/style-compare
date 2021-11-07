71d40bb9fec513afc81e87c4cb084df2c895ac89
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-07 12:33:26 +0530

    imported style img icons, changed context values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { createContext } from "react";
 import ButtonStyles from "./pages/Buttons";
+import Bootstrap from "./assets/img/bootstrap.png";
+import Material from "./assets/img/material.png";
 
 export const TypeContext = createContext();
 
 function App() {
   return (
-    <TypeContext.Provider value={["M", "B"]}>
+    <TypeContext.Provider
+      value={{ imgSrc: [Material, Bootstrap], value: ["M", "B"] }}>
       <ButtonStyles />
     </TypeContext.Provider>
   );
