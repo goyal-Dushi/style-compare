dae57e44c92b520401accb78e4a7509abf820712
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-13 11:43:56 +0530

    moved sidenavs to respective component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { createContext } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import styles from "./app.module.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./app.css";
 import ButtonStyles from "./pages/Buttons";
-import Alerts from "./modules/Alerts";
+import Alerts from "./pages/Alert";
+import LeftSideNav from "./components/LeftSideNav";
 import Bootstrap from "./assets/img/bootstrap.png";
 import Material from "./assets/img/material.png";
 import Home from "./pages/Home";
@@ -15,60 +11,25 @@ import Home from "./pages/Home";
 export const TypeContext = createContext();
 
 function App() {
-  let navigate = useNavigate();
   return (
     <>
-      <nav className={styles.header}>
+      <nav className={"header"}>
         <p> {"style-compare"} </p>
       </nav>
-      <div className={styles.bodyWrapper}>
-        <aside className={styles.sideNav}>
-          <ul className={styles.sideNavContent}>
-            <li
-              onClick={() => {
-                navigate("/");
-              }}>
-              {"Home"}
-            </li>
-            <li
-              onClick={() => {
-                navigate("/buttons");
-              }}>
-              {"Buttons"}
-            </li>
-            <li
-              onClick={() => {
-                navigate("/alerts");
-              }}>
-              {"Alerts"}
-            </li>
-          </ul>
-        </aside>
-        <div className={styles.bodyContainer}>
+      <div className={"bodyWrapper"}>
+        <LeftSideNav />
+        <div className={"bodyContainer"}>
           <TypeContext.Provider
             value={{ imgSrc: [Material, Bootstrap], value: ["M", "B"] }}>
-            <div className={styles.mainContent}>
-              <Routes>
-                <Route path={"/"} element={<Home />} />
-                <Route path={"/buttons"} element={<ButtonStyles />} />
-                <Route path={"/alerts"} element={<Alerts />} />
-              </Routes>
-            </div>
+            <Routes>
+              <Route path={"/"} element={<Home />} />
+              <Route path={"/buttons"} element={<ButtonStyles />} />
+              <Route path={"/alerts"} element={<Alerts />} />
+            </Routes>
           </TypeContext.Provider>
         </div>
-        <aside className={styles.sideNav}>
-          <ul className={styles.sideNavContent}>
-            <li> {"something1"} </li>
-            <li> {"somethin2"} </li>
-            <li> {"something3"} </li>
-            <li> {"something4"} </li>
-            <li> {"something5"} </li>
-            <li> {"something1"} </li>
-            <li> {"something5"} </li>
-          </ul>
-        </aside>
       </div>
-      <footer className={styles.footerStyle}>Copyright Footer</footer>
+      <footer className={"footerStyle"}>Copyright Footer</footer>
     </>
   );
 }
