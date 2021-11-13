9a7b040ea9f3d40ed636216fdc93a8f5c4f13d97
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-13 11:46:43 +0530

    added bootstrap sizing & position styles, included RightSideNav component

--- src/pages/Alert.js
+++ src/pages/Alert.js
@@ -1,13 +1,17 @@
 import React from "react";
+import RightSideNav from "../components/RightSideNav";
 import AlertColors from "../modules/Alerts/Colors";
 import AlertVariants from "../modules/Alerts/Variants";
 
 function Alerts() {
   return (
-    <div className={"w-100"} style={{ overflowX: "hidden" }}>
-      <h1 className={"display-4 ps-4"}>{"Alerts"}</h1>
-      <AlertColors />
-      <AlertVariants />
+    <div className={"w-100 d-flex"}>
+      <div className={"w-75"}>
+        <h1 className={"display-4 ps-4"}>{"Alerts"}</h1>
+        <AlertColors />
+        <AlertVariants />
+      </div>
+      <RightSideNav values={["Colors & Icons", "Variants"]} />
     </div>
   );
 }
--- src/pages/Buttons.js
+++ src/pages/Buttons.js
@@ -1,3 +1,4 @@
+import RightSideNav from "../components/RightSideNav";
 import ButtonColors from "../modules/Buttons/Button.colors";
 import ButtonDisabled from "../modules/Buttons/Button.Disabled";
 import ButtonSizeVariants from "../modules/Buttons/Button.sizeVariants";
@@ -5,12 +6,17 @@ import WithIcons from "../modules/Buttons/WithIcons";
 
 export default function ButtonStyles() {
   return (
-    <div className={"w-100"} style={{ overflowX: "hidden" }}>
-      <h1 className={"display-4 ps-4"}>{"Buttons"}</h1>
-      <ButtonColors />
-      <ButtonSizeVariants />
-      <ButtonDisabled />
-      <WithIcons />
+    <div className={"w-100 d-flex"}>
+      <div className={"w-75"}>
+        <h1 className={"display-4 ps-4"}>{"Buttons"}</h1>
+        <ButtonColors />
+        <ButtonSizeVariants />
+        <ButtonDisabled />
+        <WithIcons />
+      </div>
+      <RightSideNav
+        values={["Colors", "Size & Variants", "Disabled", "Icons"]}
+      />
     </div>
   );
 }
