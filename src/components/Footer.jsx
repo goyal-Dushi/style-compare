8e4fe37bfd8f21130ee45d836987ea54748a1841
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-14 17:09:47 +0530

    created navbar and footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return <footer className={styles.footerStyle}>{"Copyright Footer"}</footer>;
+}
---
+++ src/components/NavHeader.jsx
@@ -0,0 +1,9 @@
+import styles from "./navhead.module.css";
+
+export default function NavHeader() {
+  return (
+    <nav className={styles.header}>
+      <p> {"style-compare"} </p>
+    </nav>
+  );
+}
