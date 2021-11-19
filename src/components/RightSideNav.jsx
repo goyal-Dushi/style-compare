9d7db15666c1dfb57a763b57318162da66abf140
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-19 12:43:06 +0530

    updated links for scrollspy

--- src/components/RightSideNav.jsx
+++ src/components/RightSideNav.jsx
@@ -1,15 +1,19 @@
-export default function RightSideNav({ values }) {
+export default function RightSideNav({ values, scrollRef }) {
   return (
     <>
       <aside className={"position-fixed end-0 top-20"} style={{ width: "20%" }}>
         <h5 className={"mx-auto w-50"}>
           <u>{"On This Page"}</u>
         </h5>
-        <ul className={"mx-auto w-50 h-auto ps-0"}>
+        <div
+          id={"rightNav"}
+          className={"mx-auto w-50 h-auto ps-0 list-group list-group-flush"}>
           {values?.map((item, i) => (
-            <li key={i}> {item} </li>
+            <a key={i} className={"list-group-item"} href={scrollRef[i]}>
+              {item}
+            </a>
           ))}
-        </ul>
+        </div>
       </aside>
     </>
   );
