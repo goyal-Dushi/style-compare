8745d88b65232f9a95070f3a3456694b8303d6c2
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-11-21 22:09:38 +0530

    added component for chips, badges :In Progress

---
+++ src/modules/Chips&Badges/Chips/ColorsSizes.jsx
@@ -0,0 +1,107 @@
+import { Chip } from "@mui/material";
+import React, { useState } from "react";
+import * as Boot from "react-bootstrap";
+import CodeBox from "../../../components/CodeBox";
+import DescBox from "../../../components/DescBox";
+import DisplayBox from "../../../components/DisplayBox";
+import StyleToggleNav from "../../../components/StyleToggleNav";
+
+export default function ColorsSizes() {
+  const [type, setType] = useState("M");
+  let data;
+  const getHTML = () => {
+    if (type === "M") {
+      data = `
+    <Chip label="Default Chip" />
+    <Chip label="Primary" color="primary" variant="outlined" />
+    <Chip label="Secondary" color="secondary" variant="outlined" />
+    <Chip label="Error" color="error" variant="outlined" />
+    <Chip label="Info" color="info" size="small" variant="filled" />
+    <Chip label="Success" color="success" size="small" />
+    <Chip label="Warning" color="warning" size="small" />
+    `;
+      return (
+        <>
+          <Chip label={"Default Chip"} />
+          <Chip label={"Primary"} color={"primary"} variant={"outlined"} />
+          <Chip label={"Secondary"} color={"secondary"} variant={"outlined"} />
+          <Chip label={"Error"} color={"error"} variant={"outlined"} />
+          <Chip label={"Info"} color={"info"} size={"small"} />
+          <Chip label={"Success"} color={"success"} size={"small"} />
+          <Chip label={"Warning"} color={"warning"} size={"small"} />
+        </>
+      );
+    } else if (type === "B") {
+      data = `
+    <Badge > Primary Default </Badge>
+    <Badge bg="success" className="fs-1" > Success </Badge>
+    <Badge bg="danger" className="fs-2" > Danger </Badge>
+    <Badge pill bg="warning" className="fs-3" > Warning! </Badge>
+    <Badge pill bg="info" className="fs-4" > Info </Badge>
+    <Badge pill bg="light" className="fs-5" > Light </Badge>
+    <Badge pill bg="dark" className="fs-6" > Dark </Badge>
+    `;
+      return (
+        <>
+          <Boot.Badge> {"Primary Default"} </Boot.Badge>
+          <Boot.Badge className={"fs-1"} bg={"success"}>
+            {"Success"}
+          </Boot.Badge>
+          <Boot.Badge className={"fs-2"} bg={"danger"}>
+            {"Danger"}
+          </Boot.Badge>
+          <Boot.Badge pill className={"fs-3"} bg={"warning"}>
+            {"Warning!"}
+          </Boot.Badge>
+          <Boot.Badge pill className={"fs-4"} bg={"info"}>
+            {"Info"}
+          </Boot.Badge>
+          <Boot.Badge className={"text-secondary fs-5"} pill bg={"light"}>
+            {"Light"}
+          </Boot.Badge>
+          <Boot.Badge className={"fs-6"} pill bg={"dark"}>
+            {"Dark"}
+          </Boot.Badge>
+        </>
+      );
+    }
+  };
+
+  const getContent = () => {
+    if (type === "M") {
+      return (
+        <>
+          <b> Material </b> has separate component named
+          <span className='codeSnippet'>{"<Chip/>"}</span>
+          for rendering chips.By Default, the Chip has
+          <span className='codeSnippet'>size:medium</span>,
+          <span className='codeSnippet'>color:default</span> and
+          <span className='codeSnippet'>variant:filled</span>.
+        </>
+      );
+    } else if (type === "B") {
+      return (
+        <>
+          In
+          <b> Bootstrap </b>, you use the same
+          <span className='codeSnippet'>{"<Badge/>"}</span> for both chips and
+          badges. By Default, it has background,
+          <span className='codeSnippet'>bg:primary</span> and no rounded
+          borders. Bootstrap dosen't provide any size property/attribute for its
+          Badge component, but you can trigger different sized by utilizing the
+          <span className='codeSnippet'>{"fs-[x]"}</span>, where 'x' ranges from
+          {"[1,6]"}, 1 being largest and 6 being smallest.
+        </>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <DescBox heading={"Colors, Sizes & Variants"} content={getContent()} />
+      <StyleToggleNav setStyleType={setType} />
+      <DisplayBox> {getHTML()} </DisplayBox>
+      <CodeBox snippet={data} />
+    </div>
+  );
+}
---
+++ src/modules/Chips&Badges/Chips/IconChips.jsx
@@ -0,0 +1,159 @@
+import { AddCircle, AddCircleOutline, Delete } from "@mui/icons-material";
+import { Chip, Button, Avatar } from "@mui/material";
+import * as Boot from "react-bootstrap";
+import React, { useState } from "react";
+import CodeBox from "../../../components/CodeBox";
+import DisplayBox from "../../../components/DisplayBox";
+import StyleToggleNav from "../../../components/StyleToggleNav";
+import DescBox from "../../../components/DescBox";
+
+function IconChips() {
+  const [type, setType] = useState("M");
+  const [showChip, setShowChip] = useState(true);
+  const [showChip2, setShowChip2] = useState(true);
+
+  let data;
+  const handleDelete = () => {
+    setShowChip(false);
+  };
+  const handleDelete2 = () => {
+    setShowChip2(false);
+  };
+  const getHTML = () => {
+    data = `
+    <Chip clickable icon={<AddCircle />} label="Add Item" color="primary" />
+    <Chip clickable icon={<AddCircleOutline />} label="Add Item" variant="outlined" color="success" />
+    <Chip clickable avatar={<Avatar>M</Avatar>} label="Avatar" color="primary" />
+    <Chip deleteIcon={<Delete />} label="Delete Chip" onDelete={handleDelete} variant="filled" color="error" />
+    <Chip label=" Default Delete Chip" onDelete={handleDelete2} color="primary" />
+    `;
+    if (type === "M") {
+      return (
+        <>
+          <Chip
+            clickable
+            icon={<AddCircle />}
+            label={"Add Item"}
+            color={"primary"}
+          />
+          <Chip
+            clickable
+            icon={<AddCircleOutline />}
+            label={"Add Item"}
+            variant={"outlined"}
+            color={"success"}
+          />
+          <Chip
+            clickable
+            avatar={<Avatar>{"M"}</Avatar>}
+            label={"Avatar"}
+            color={"secondary"}
+          />
+          {showChip ? (
+            <Chip
+              deleteIcon={<Delete />}
+              label={"Delete Chip"}
+              onDelete={handleDelete}
+              variant={"filled"}
+              color={"error"}
+            />
+          ) : (
+            <Button
+              onClick={() => setShowChip(true)}
+              variant={"contained"}
+              color={"success"}
+              size={"small"}>
+              {"Show"}
+            </Button>
+          )}
+          {showChip2 ? (
+            <Chip
+              label={" Default Delete Chip"}
+              onDelete={handleDelete2}
+              color={"primary"}
+            />
+          ) : (
+            <Button
+              onClick={() => setShowChip2(true)}
+              variant={"contained"}
+              color={"success"}
+              size={"small"}>
+              {"Show Default"}
+            </Button>
+          )}
+        </>
+      );
+    } else if (type === "B") {
+      data = `
+    <Badge pill bg="success" className="fs-5"><i className="bi bi-check-circle-fill"></i> Success! </Badge>
+    <Badge pill bg="danger" className="fs-5"> Delete! <i className="bi bi-trash-fill" onClick={() => handleDelete} ></i> </Badge>
+    `;
+      return (
+        <>
+          <Boot.Badge pill bg={"success"} className={"fs-5"}>
+            <i className={"bi bi-check-circle-fill"}></i> {"Success "}
+          </Boot.Badge>
+          {showChip ? (
+            <Boot.Badge pill className={"fs-5"} bg={"danger"}>
+              {"Delete "}
+              <i className={"bi bi-trash-fill"} onClick={handleDelete}></i>
+            </Boot.Badge>
+          ) : (
+            <Boot.Button
+              onClick={() => setShowChip(true)}
+              variant={"primary"}
+              size={"sm"}>
+              {"Show"}
+            </Boot.Button>
+          )}
+        </>
+      );
+    }
+  };
+
+  const getContent = () => {
+    if (type === "M") {
+      return (
+        <>
+          <b>Material</b> has properties/attributes namely
+          <span className='codeSnippet'>icon</span>,
+          <span className='codeSnippet'>deleteIcon</span> and{" "}
+          <span className='codeSnippet'>avatar</span>.
+          <span className='codeSnippet'>icon</span> property takes an material
+          icon component import from
+          <span className='codeSnippet'>@mui/icons-material</span> library and
+          by default is placed at the start of the chip, whereas, the
+          <span className='codeSnippet'>deleteIcon</span> places the icon
+          component at the end of the chip.
+          <span className='codeSnippet'>avatar</span> is used if you want to
+          place your own media files in the chip. Also, if the
+          <span className='codeSnippet'>onDelete</span> property is specified,
+          only then the deleteIcon is shown and by default a
+          <span className='codeSnippet'>cross</span> icon is displayed by
+          material. You could also set
+          <span className='codeSnippet'>clickable</span> property to the chip to
+          have the ripple effect provided by Material.
+        </>
+      );
+    } else if (type === "B") {
+      return (
+        <>
+          <b>Bootstrap</b> dosen't provide any property/attribute for its
+          component to place icon. But, you can use the
+          <span className='codeSnippet'>{"<i></i>"}</span> tags to place
+          bootstrap icons at your desired position as shown below.
+        </>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <DescBox heading={"Icon Chips/Chip Actions"} content={getContent()} />
+      <StyleToggleNav setStyleType={setType} />
+      <DisplayBox> {getHTML()} </DisplayBox>
+      <CodeBox snippet={data} />
+    </div>
+  );
+}
+export default IconChips;
