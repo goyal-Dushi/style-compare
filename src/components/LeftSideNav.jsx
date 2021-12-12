1f39a7194930a6d1263b285270e0f3091501acfd
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-12-12 17:41:26 +0530

    added accordion component

--- src/components/LeftSideNav.jsx
+++ src/components/LeftSideNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Accordion from "react-bootstrap/Accordion";
 import { useNavigate } from "react-router";
 import styles from "./leftSideNav.module.css";
 
@@ -8,30 +9,44 @@ export default function LeftSideNav() {
     <>
       <aside className={styles.sideNav}>
         <ul className={`${styles.sideNavContent} w-75 mx-auto`}>
-          <li
-            onClick={() => {
-              navigate("/");
-            }}>
-            {"Home"}
-          </li>
-          <li
-            onClick={() => {
-              navigate("/buttons");
-            }}>
-            {"Buttons"}
-          </li>
-          <li
-            onClick={() => {
-              navigate("/alerts");
-            }}>
-            {"Alerts"}
-          </li>
-          <li
-            onClick={() => {
-              navigate("/chips&badges");
-            }}>
-            {"Chips/Badges"}
-          </li>
+          <Accordion flush>
+            <Accordion.Item eventKey={"0"}>
+              <Accordion.Header>
+                <li>{"Getting Started"}</li>
+              </Accordion.Header>
+              <Accordion.Body>
+                <li
+                  onClick={() => {
+                    navigate("/");
+                  }}>
+                  {"About"}
+                </li>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey={"1"}>
+              <Accordion.Header>{"Components"}</Accordion.Header>
+              <Accordion.Body>
+                <li
+                  onClick={() => {
+                    navigate("/buttons");
+                  }}>
+                  {"Buttons"}
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/alerts");
+                  }}>
+                  {"Alerts"}
+                </li>
+                <li
+                  onClick={() => {
+                    navigate("/chips&badges");
+                  }}>
+                  {"Chips/Badges"}
+                </li>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </ul>
       </aside>
     </>
