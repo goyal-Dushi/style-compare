7358af701cd7ad7aea4cea30a188fd52a6e225bc
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-12-19 17:46:11 +0530

    added Ref for content in <p> to render HTML

--- src/components/DescBox.jsx
+++ src/components/DescBox.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 export default function DescBox({ heading, content }) {
+  const contentRef = useRef();
+
+  useEffect(() => {
+    contentRef.current.innerHTML = content;
+  }, [content]);
+
   return (
     <div style={{ margin: "40px 40px 0px" }}>
       <h2 className={"display-6"}> {heading} </h2>
-      <p>{content}</p>
+      <p ref={contentRef}></p>
     </div>
   );
 }
