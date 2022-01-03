d4f6115a38fe4b6f6997841bd1791b0d516e6d57
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-01-03 22:47:20 +0530

    added installation page

---
+++ src/pages/Installation.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import CodeBox from "../components/CodeBox";
+import DescBox from "../components/DescBox";
+import StyleToggleNav from "../components/StyleToggleNav";
+
+export default function Installation() {
+  const [type, setType] = useState("M");
+
+  const getSnippet = () => {
+    switch (type) {
+      case "M":
+        return `
+    npm install @mui/material @emotion/react @emotion/styled
+
+    yarn add @mui/material @emotion/react @emotion/styled
+        `;
+      case "B":
+        return `
+    npm install bootstrap
+
+    yarn add bootstrap
+            `;
+      default:
+        return "To be updated soon!";
+    }
+  };
+
+  const getHeading = () => {
+    switch (type) {
+      case "M":
+        return "Material";
+      case "B":
+        return "Bootstrap";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div className={"w-75"}>
+      <h1 className={"display-4 fw-bold"}> {"Installation"} </h1>
+      <DescBox
+        heading={getHeading()}
+        content={
+          "You can use either yarn or npm, most popular node package manager libs to install such dependencies in your next React/Vue/Angular project."
+        }
+      />
+      <StyleToggleNav setStyleType={setType} />
+      <CodeBox snippet={getSnippet()} />
+    </div>
+  );
+}
