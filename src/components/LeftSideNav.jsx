b7ab7fe681b4ba56b484815e1d60e9e5cec19e59
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-01-08 18:08:06 +0530

    added active status for clicked links, added json data for content

--- src/components/LeftSideNav.jsx
+++ src/components/LeftSideNav.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { useNavigate } from "react-router";
 import styles from "./leftSideNav.module.css";
+import { options } from "./leftNavOptions.json";
 
 export default function LeftSideNav() {
+  const [selectOption, setSelected] = useState("About");
+
+  const handleOptionClick = (e) => {
+    console.log(e.currentTarget.innerText);
+    setSelected(e.currentTarget.innerText);
+  };
+
   let navigate = useNavigate();
   return (
     <>
@@ -15,47 +24,49 @@ export default function LeftSideNav() {
                 <li>{"Getting Started"}</li>
               </Accordion.Header>
               <Accordion.Body>
-                <li
-                  onClick={() => {
-                    navigate("/");
-                  }}>
-                  {"About"}
-                </li>
-                <li
-                  onClick={() => {
-                    navigate("/installation");
-                  }}>
-                  {"Installation"}
-                </li>
+                <ul className='list-group list-group-flush'>
+                  {options.start.map((item, i) => {
+                    return (
+                      <li
+                        key={i}
+                        className={
+                          selectOption === item.name
+                            ? "list-group-item list-group-item-action py-2 list-group-item-info"
+                            : "list-group-item list-group-item-action py-2"
+                        }
+                        onClick={(e) => {
+                          navigate(item.link);
+                          handleOptionClick(e);
+                        }}>
+                        {item.name}
+                      </li>
+                    );
+                  })}
+                </ul>
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey={"1"}>
               <Accordion.Header>{"Components"}</Accordion.Header>
               <Accordion.Body>
-                <li
-                  onClick={() => {
-                    navigate("/buttons");
-                  }}>
-                  {"Buttons"}
-                </li>
-                <li
-                  onClick={() => {
-                    navigate("/alerts");
-                  }}>
-                  {"Alerts"}
-                </li>
-                <li
-                  onClick={() => {
-                    navigate("/chips&badges");
-                  }}>
-                  {"Chips/Badges"}
-                </li>
-                <li
-                  onClick={() => {
-                    navigate("/progress");
-                  }}>
-                  {"Progress"}
-                </li>
+                <ul className='list-group list-group-flush'>
+                  {options.components.map((item, i) => {
+                    return (
+                      <li
+                        key={i}
+                        className={
+                          selectOption === item.name
+                            ? "list-group-item list-group-item-action py-2 list-group-item-info"
+                            : "list-group-item list-group-item-action py-2"
+                        }
+                        onClick={(e) => {
+                          navigate(item.link);
+                          handleOptionClick(e);
+                        }}>
+                        {item.name}
+                      </li>
+                    );
+                  })}
+                </ul>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
