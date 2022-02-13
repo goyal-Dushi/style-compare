9734def7a5c6dd4322b9db6917acc3c3e352fba0
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-02-13 15:54:03 +0530

    created typecontext file for style toggle nav

---
+++ src/libtype.js
@@ -0,0 +1,8 @@
+import Bootstrap from "./assets/img/bootstrap.webp";
+import Material from "./assets/img/material.webp";
+
+export const libType = {
+  imgSrc: [Material, Bootstrap],
+  value: ["M", "B"],
+  names: ["Material", "Bootstrap"],
+};
