dd3f73eb5e21f9032a975eb0c9082e2736a66ba6
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-02-13 15:58:16 +0530

    added highlight on select style

--- src/components/RightSideNav.jsx
+++ src/components/RightSideNav.jsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
+
 export default function RightSideNav({ values, scrollRef }) {
+  const [selectedText, setSelectedText] = useState(values[0]);
+  const handleOptionSelect = (e) => {
+    setSelectedText(e.currentTarget.innerText);
+  };
+
   return (
     <>
       <aside className={"position-fixed end-0 top-20"} style={{ width: "20%" }}>
         <h5 className={"mx-auto w-50"}>
           <u>{"On This Page"}</u>
         </h5>
-        <div
+        <ul
           id={"rightNav"}
           className={"mx-auto w-50 h-auto ps-0 list-group list-group-flush"}>
           {values?.map((item, i) => (
-            <a key={i} className={"list-group-item"} href={scrollRef[i]}>
-              {item}
+            <a
+              key={i}
+              onClick={(e) => handleOptionSelect(e)}
+              style={{ textDecoration: "none" }}
+              href={scrollRef[i]}>
+              <li
+                className={
+                  selectedText === item
+                    ? "list-group-item list-group-item-action list-group-item-info"
+                    : "list-group-item list-group-item-action"
+                }>
+                {item}
+              </li>
             </a>
           ))}
-        </div>
+        </ul>
       </aside>
     </>
   );
