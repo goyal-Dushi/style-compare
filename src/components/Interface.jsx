06b957d00797aea9c1cc66e5bb80a2aef9b9a02e
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-02-19 21:02:34 +0530

    created generic interface component

---
+++ src/components/Interface.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import DescBox from "./DescBox";
+import StyleToggleNav from "./StyleToggleNav";
+import DisplayBox from "./DisplayBox";
+import CodeBox from "./CodeBox";
+
+export default function Interface(props) {
+  const { componentID, heading, content, setType, setHtml, codeData } = props;
+
+  return (
+    <>
+      <div id={componentID}>
+        <DescBox heading={heading} content={content} />
+        <StyleToggleNav setStyleType={setType} />
+        <DisplayBox> {setHtml()} </DisplayBox>
+        <CodeBox snippet={codeData?.current} />
+      </div>
+    </>
+  );
+}
