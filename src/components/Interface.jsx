d21b7bc91a7fce568a7e7b5d7dd6bf9fab8a8e4f
goyal-Dushi <dushyantgoyal28@gmail.com>
2022-03-19 21:49:56 +0530

    added boolean to display editor

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -3,17 +3,20 @@ import DescBox from './DescBox';
 import StyleToggleNav from './StyleToggleNav';
 import DisplayBox from './DisplayBox';
 import CodeBox from './CodeBox';
+import CodeSandBox from './CodeSandBox';
 
 export default function Interface(props) {
   const { componentID, heading, content, setType, setHtml, codeData, linkTo } =
     props;
   const linkState = useRef(linkTo);
+  const showEditor = Boolean(componentID);
   return (
     <>
       <div id={componentID || ''}>
         {heading && content && <DescBox heading={heading} content={content} />}
         <StyleToggleNav setStyleType={setType} linkTo={linkState?.current} />
         {setHtml ? <DisplayBox> {setHtml()} </DisplayBox> : <></>}
+        <CodeSandBox show={showEditor} />
         {codeData.current ? (
           <CodeBox snippet={codeData.current} />
         ) : (
